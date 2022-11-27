6696bb23616c86c9cb5b2b1b5981a70e403cc761
Allie c( ⁰ 〰 ⁰ )੭ <azhang@users.noreply.github.com>
2022-11-28 04:40:42 +0900

    remove sharp and add setup for cloudflare pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    runtime: 'experimental-edge', // 'node.js' (default) | 'experimental-edge'
     appDir: true,
   },
 }

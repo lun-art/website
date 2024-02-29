75cc46c1df8aa8115bc2ca3e22edb7cd3cbdd735
Allie c( ⁰ 〰 ⁰ )੭ <azhang@users.noreply.github.com>
2024-03-01 04:32:15 +0800

    favicon fix

--- app/head.tsx
+++ app/head.tsx
@@ -1,6 +1,5 @@
 import copy from "./copy"
 import heroImg from '../public/PIA12936.jpg'
-import logo from '../public/logo.svg'
 
 export default function Head() {
   return (
@@ -8,7 +7,6 @@ export default function Head() {
       <title>{`${copy.title0} ${copy.title1}`}</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content={copy.description} />
-      <link rel="icon" href={logo.src} sizes="any" type="image/svg+xml" />
 
       <meta property="og:title" content={`${copy.title0} ${copy.title1}`} />
       <meta
